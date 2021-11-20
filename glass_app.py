1cb056aed6e8952c4e4fc0cc2be574a191740f7c
estrellaorozco018 <90882167+estrellaorozco018@users.noreply.github.com>
2021-11-20 10:49:42 -0800

    Update glass_app.py

--- glass_app.py
+++ glass_app.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import plot_confusion_matrix
